99e56102e6626d01897bb5e3343f8d714ead2305
Dragonzion <alexandre02a@hotmail.com>
2026-08-11 20:15:11 +0100

    added tick clock, ruff cleanup

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import pygame
 
 import constants
-
 from logger import log_state
-
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
-    pygame.init()
+    _ = pygame.init()
 
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+        _ = screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 #noqa
 
 
 if __name__ == "__main__":
