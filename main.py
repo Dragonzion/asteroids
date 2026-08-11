284fe0ad8b88795f7d3c7c4844802616a8b0a361
Dragonzion <alexandre02a@hotmail.com>
2026-08-11 20:44:12 +0100

    added hitboxes, created player class, updated draw screen to draw player objects

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 import constants
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -11,6 +12,7 @@ def main():
     _ = pygame.init()
 
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    player = Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
 
     clock = pygame.time.Clock()
     dt = 0.0
@@ -21,8 +23,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         _ = screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000 #noqa
+
 
 
 if __name__ == "__main__":
