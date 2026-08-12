63f33310bcbcc503d4cc79b955ac2268c1f394ad
Dragonzion <alexandre02a@hotmail.com>
2026-08-12 18:51:09 +0100

    added player movement, object groups

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     _ = pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable  = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    player = Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
+    player = Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2) #noqa
 
     clock = pygame.time.Clock()
     dt = 0.0
@@ -23,8 +26,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         _ = screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
