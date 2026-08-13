2a65e48c9333ef74929d442ffc2b2ae54685f794
Dragonzion <alexandre02a@hotmail.com>
2026-08-13 02:35:32 +0100

    added shooting

--- main.py
+++ main.py
@@ -7,27 +7,41 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from logger import log_event, log_state
 from player import Player
+from shot import Shot
 
 
 def main():
+    #Console Prints
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    #Start the game
     _ = pygame.init()
+
+    #Creating groups
     updatable = pygame.sprite.Group()
     drawable  = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    #Setting containers
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots,updatable, drawable)
 
+    #Creating game objects
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     player = Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
     asteroid_field = AsteroidField() #noqa creating asteroid field object
 
+
+    #Creating game clock and delta time
     clock = pygame.time.Clock()
     dt = 0.0
 
+    #Game loop
     while True:
         log_state()
         for event in pygame.event.get():
